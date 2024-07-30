79558c8f005051210f6850610499a2e1e9a9a6d1
jfandre00 <jfandre@gmail.com>
2024-07-30 09:52:52 -0300

    aula de 29-07-24

--- Semana 01/Trilha de Aprendizagem 01/exercícios_trilha02.js
+++ Semana 01/Trilha de Aprendizagem 01/exercícios_trilha02.js
@@ -1,30 +1,55 @@
-//Criação através de construtor de objetos
+// Criação através de construtor de objetos
 
-function Produto(nome, preco, quantidade) 
-    {
-        this.nome = nome;
-        this.preco = preco;
-        this.quantidade = quantidade;
+function Produto(nome, preco, quantidade) {
+    this.nome = nome;
+    this.preco = preco;
+    this.quantidade = quantidade;
+}
+
+function Venda(id, data, produto) {
+    this.id = id;
+    this.data = data;
+    this.produto = produto;
+    this.total = function() {
+        return this.produto.preco * this.produto.quantidade;
+    };
+}
+
+function Carrinho() {
+    this.produtos = [];
+
+    this.adicionarProduto = function(produto) {
+        this.produtos.push(produto);
     };
 
-function Venda (id, data, produto)
-    {
-        this.id = id;
-        this.data = data;
-        this.produto = produto;
-        this.total = function() {
-            return this.produto.preco * this.produto.quantidade;
-        }  
+    this.removerProduto = function(produto) {
+        // Encontrar o índice do produto e removê-lo
+        const index = this.produtos.findIndex(p => p.nome === produto.nome);
+        if (index !== -1) {
+            this.produtos.splice(index, 1);
+        }
     };
+
+    this.listarProdutos = function() {
+        // Listar os produtos do carrinho
+        return this.produtos.map(p => `Produto: ${p.nome}, Preço: ${p.preco}, Quantidade: ${p.quantidade}`).join("\n");
+    };
+}
 
 let celular = new Produto("iPhone", 2500, 1);
-
-console.log(celular.preco);
+let capa = new Produto("Capa", 200, 1);
+let pelicula = new Produto("Pelicula", 50, 4);
 
 let venda01 = new Venda(12345, "2024-10-10", celular);
-console.log(celular);
-console.log(venda01.id);
-console.log(venda01.produto);
-console.log(venda01.produto.preco);
-console.log(venda01.data);
-console.log(venda01.total())
+
+const meuCarrinho = new Carrinho();
+
+meuCarrinho.adicionarProduto(celular);
+meuCarrinho.adicionarProduto(capa);
+meuCarrinho.adicionarProduto(pelicula);
+
+console.log(meuCarrinho.listarProdutos());
+
+meuCarrinho.removerProduto(capa);
+
+console.log(meuCarrinho.listarProdutos());
---
+++ Semana 01/Trilha de Aprendizagem 01/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+console.log(celular);
+console.log(venda01.id);
+console.log(venda01.produto);
+console.log(venda01.produto.preco);
+console.log(venda01.data);
+console.log(venda01.total());
+console.log("Testando o carrinho");
